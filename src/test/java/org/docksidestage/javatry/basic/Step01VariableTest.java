9de7393cd465e1aaa87346da81209c186e228b42
kazutoshi-mizutani-biz <kazutoshi.mizutani@bizreach.co.jp>
2020-04-22 11:12:37 +0900

    Step01 test_variable_method_argument_variable_assignment

--- src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
@@ -167,7 +167,8 @@ public class Step01VariableTest extends PlainTestCase {
         StringBuilder sea = new StringBuilder("harbor");
         int land = 415;
         helpMethodArgumentVariable(sea, land);
-        log(sea); // your answer? => 
+        log(sea); // your answer? => harbor416
+//        correct answer => harbor
     }
 
     private void helpMethodArgumentVariable(StringBuilder sea, int land) {
