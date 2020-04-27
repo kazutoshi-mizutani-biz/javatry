d9bb76d766b6bdb99a3bb803791a7d7529a09c77
kazutoshi-mizutani-biz <kazutoshi.mizutani@bizreach.co.jp>
2020-04-27 10:54:26 +0900

    Step07 test_exception_translation_improveChallenge

--- src/main/java/org/docksidestage/bizfw/basic/screw/SpecialScrewManufacturer.java
+++ src/main/java/org/docksidestage/bizfw/basic/screw/SpecialScrewManufacturer.java
@@ -24,7 +24,7 @@ public class SpecialScrewManufacturer {
     public SpecialScrew makeSpecialScrew(ScrewSpec screwSpec) {
         String specText = screwSpec.getSpecText();
         if (specText.equals("\\(^_^)/")) { // too pinpoint!?
-            String msg = "The kawaii face is not useful to make screw: " + screwSpec;
+            String msg = "The substituted spec is illegal: " + screwSpec;
             throw new SpecialScrewCannotMakeBySpecException(msg);
         }
         return new SpecialScrew(specText);
